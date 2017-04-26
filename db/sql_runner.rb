70b067af544b2bdba1b656be0925c3fe22b7d052
Charlie Wood <charliemowood@gmail.com>
2017-04-26 18:51:28 +0100

    Modify SqlRunner

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -1,15 +1,15 @@
 require(PG)
 class SqlRunner
 
-  def run(sql)
+  def SqlRunner.run(sql)
 
     begin
       db = PG.connect ({dbname: 'music', host: 'localhost'})
-      db.exec(sql)
+      result = db.exec(sql)
     ensure
       db.close()
     end
+
+    return result
   end
-
-
 end
